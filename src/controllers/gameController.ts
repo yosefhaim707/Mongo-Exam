4f6f3e9c7a96879f644006ae98b54c4426b1f5ac
Yosef Haim <yosefhaimmalamud@gmail.com>
2024-10-07 18:22:31 +0300

    started the game controller

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -1,2 +1,7 @@
-import Game from "../models/Game";
+import Game, { IGame } from "../models/Game";
 import { Request, Response, NextFunction } from "express";
+import jsonWebToken from 'jsonwebtoken';
+
+export const getGames = async (): Promise<void> => {
+    await Game.find();
+}
